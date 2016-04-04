d38d4da5bf915663e17f5a1a28fcce8c0fec2ca9
IMIDSAC <imidsac@hotmail.fr>
2016-04-04 11:51:17 +0000

    Add scepe and some functions in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,31 @@ class User < ActiveRecord::Base
   has_many :section_edits
   has_many :sections, :through => :section_edits
 
+  #validate :username_is_allowed
+  #validate :no_new_users_on_saturday, :on => :create
 
+  scope :sorted, lambda { order("prenom ASC, nom ASC") }
+
+  def name
+    "#{nom} #{prenom}"
+    # Or: first_name + ' ' + last_name
+    # Or: [first_name, last_name].join(' ')
+  end
+
+  private
+
+  def username_is_allowed
+    if FORBIDDEN_USERNAMES.include?(username)
+      errors.add(:username, "has been restricted from use.")
+    end
+  end
+
+  # Errors not related to a specific attribute
+  # can be added to errors[:base]
+  def no_new_users_on_saturday
+    if Time.now.wday == 6
+      errors[:base] << "No new users on Saturdays."
+    end
+  end
 
 end
