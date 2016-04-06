0330d7a90ce33b13c604c242c983928b71350b8e
IMIDSAC <imidsac@hotmail.fr>
2016-04-06 17:52:20 +0000

    Add confirmable and custom new user.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: {
                  registrations: 'users/registrations',
+                 confirmations: 'users/confirmations',
                  sessions: 'users/sessions',
                  passwords: 'users/passwords'
              },
